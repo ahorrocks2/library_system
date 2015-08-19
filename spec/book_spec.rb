b9685a55cb332dcd1a9cc2a88432a41e523a330f
Alyssa Horrocks & Carlo DeGregorio & Kellen Kolbeck <student@epicodus.com>
2015-08-19 14:47:13 -0700

    Create spec helper

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,14 +1,4 @@
-require('rspec')
-require('book')
-require('pg')
-
-DB = PG.connect({:dbname => "library_system_test"})
-
-RSpec.configure do |config|
-  config.after(:each) do
-    DB.exec("DELETE FROM books *;")
-  end
-end
+require('spec_helper')
 
 describe(Book) do
   describe('#book_title') do

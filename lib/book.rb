bae0cbf684f15d218cb23fd5e67ed3a97132605e
Alyssa Horrocks & Carlo DeGregorio & Kellen Kolbeck <student@epicodus.com>
2015-08-19 14:13:13 -0700

    it finds books by author, title,genre or id. 8 passing

--- lib/book.rb
+++ lib/book.rb
@@ -9,5 +9,65 @@ class Book
     @book_id = attributes.fetch(:book_id)
   end
 
-  
+  define_singleton_method(:all) do
+    returned_books = DB.exec("SELECT * FROM books;")
+    book_list = []
+    returned_books.each() do |book|
+      book_title = book.fetch('title')
+      book_author = book.fetch('author')
+      book_genre = book.fetch('genre')
+      book_id = book.fetch('book_id').to_i()
+      book_list.push(Book.new({:book_title => book_title, :book_author => book_author, :book_genre => book_genre, :book_id => book_id}))
+    end
+    book_list
+  end
+
+  define_method(:save) do
+    result = DB.exec("INSERT INTO books (author, title, genre) VALUES ('#{@book_author}', '#{@book_title}', '#{@book_genre}') RETURNING book_id;")
+    @book_id = result.first().fetch("book_id").to_i()
+  end
+
+  define_method(:==) do |another_book|
+    self.book_title == another_book.book_title() && self.book_author == another_book.book_author()
+  end
+
+  define_singleton_method(:find_title) do |title|
+    found_book = nil
+    Book.all().each() do |book|
+      if book.book_title() == title
+        found_book = book
+      end
+    end
+    found_book
+  end
+
+  define_singleton_method(:find_author) do |author|
+    found_book = nil
+    Book.all().each() do |book|
+      if book.book_author() == author
+        found_book = book
+      end
+    end
+    found_book
+  end
+
+  define_singleton_method(:find_genre) do |genre|
+    found_book = nil
+    Book.all().each() do |book|
+      if book.book_genre() == genre
+        found_book = book
+      end
+    end
+    found_book
+  end
+
+  define_singleton_method(:find_book_id) do |id|
+    found_book = nil
+    Book.all().each() do |book|
+      if book.book_id() == id
+        found_book = book
+      end
+    end
+    found_book
+  end
 end
--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -20,4 +20,66 @@ describe(Book) do
       expect(test_book.book_id().to_i).to(be_instance_of(Fixnum))
     end
   end
+
+  describe('.all') do
+    it('will return an empty array at first') do
+      expect(Book.all()).to(eq([]))
+    end
+  end
+
+  describe('#save') do
+    it('will save the books in the library_system database') do
+      test_book = Book.new({:book_title => 'Moby Dick', :book_author => 'Herman Melville', :book_genre => 'Fiction', :book_id => nil})
+      test_book.save()
+      expect(Book.all()).to(eq([test_book]))
+    end
+  end
+
+  describe('.==') do
+    it('will override the equal operator') do
+      test_book1 = Book.new({:book_title => 'The Great Gatsby', :book_author => 'Fitzgerald', :book_genre => 'Fiction', :book_id => nil})
+      test_book2 = Book.new({:book_title => 'The Great Gatsby', :book_author => 'Fitzgerald', :book_genre => 'Fiction', :book_id => nil})
+      expect(test_book1).to(eq(test_book2))
+    end
+  end
+
+  describe('.find_title') do
+    it('will return a book given the title') do
+      test_book1 = Book.new({:book_title => 'The Great Gatsby', :book_author => 'F. Scott Fitzgerald', :book_genre => 'Fiction', :book_id => nil})
+      test_book2 = Book.new({:book_title => 'The Bell Jar', :book_author => 'Sylvia Plath', :book_genre => 'Non-Fiction', :book_id => nil})
+      test_book1.save()
+      test_book2.save()
+      expect(Book.find_title('The Bell Jar')).to(eq(test_book2))
+    end
+  end
+
+  describe('.find_author') do
+    it('will return a books given the author') do
+      test_book1 = Book.new({:book_title => 'The Great Gatsby', :book_author => 'F. Scott Fitzgerald', :book_genre => 'Fiction', :book_id => nil})
+      test_book2 = Book.new({:book_title => 'The Bell Jar', :book_author => 'Sylvia Plath', :book_genre => 'Non-Fiction', :book_id => nil})
+      test_book1.save()
+      test_book2.save()
+      expect(Book.find_author('F. Scott Fitzgerald')).to(eq(test_book1))
+    end
+  end
+
+  describe('.find_genre') do
+    it('will return books given the genre') do
+      test_book1 = Book.new({:book_title => 'The Great Gatsby', :book_author => 'F. Scott Fitzgerald', :book_genre => 'Fiction', :book_id => nil})
+      test_book2 = Book.new({:book_title => 'The Bell Jar', :book_author => 'Sylvia Plath', :book_genre => 'Non-Fiction', :book_id => nil})
+      test_book1.save()
+      test_book2.save()
+      expect(Book.find_genre('Fiction')).to(eq(test_book1))
+    end
+  end
+
+  describe('.find_book_id') do
+    it('will return a book given the id') do
+      test_book1 = Book.new({:book_title => 'The Great Gatsby', :book_author => 'F. Scott Fitzgerald', :book_genre => 'Fiction', :book_id => nil})
+      test_book2 = Book.new({:book_title => 'The Bell Jar', :book_author => 'Sylvia Plath', :book_genre => 'Non-Fiction', :book_id => nil})
+      test_book1.save()
+      test_book2.save()
+      expect(Book.find_book_id(test_book1.book_id.to_i)).to(eq(test_book1))
+    end
+  end
 end

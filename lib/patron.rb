68f2641d8b0f14c7bcd1875ef39994a945abd8af
Alyssa Horrocks & Carlo DeGregorio & Kellen Kolbeck <student@epicodus.com>
2015-08-19 14:55:51 -0700

    Add ability to list every patron.

--- lib/patron.rb
+++ lib/patron.rb
@@ -8,6 +8,17 @@ attr_reader(:name, :book_id, :patron_id)
     @book_id = attributes.fetch(:book_id)
   end
 
+  define_singleton_method(:all) do
+    returned_patrons = DB.exec("SELECT * FROM patrons;")
+    patrons = []
+    returned_patrons.each() do |patron|
+      name = patron.fetch('name')
+      patron_id = patron.fetch('patron_id').to_i()
+      book_id = patron.fetch('book_id').to_i()
+      patrons.push(Patron.new({:name => name, :patron_id => patron_id, :book_id => book_id}))
+    end
+    patrons
+  end
 
 
 
--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -1,10 +1,17 @@
 require('spec_helper')
 
-describe Patron do
+describe(Patron) do
   describe('#name') do
     it("returns the name of the patron") do
     test_patron = Patron.new(:name => 'Ricky Bobby', :patron_id => nil, :book_id => 1)
     expect(test_patron.name()).to(eq('Ricky Bobby'))
     end
   end
+
+  describe('.all') do
+    it('returns an empty array at first') do
+      expect(Patron.all()).to(eq([]))
+    end
+  end
+
 end

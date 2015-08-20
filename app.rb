bdb507c10c6e947b51d595be7d7138ef014ef12d
Alyssa Horrocks & Carlo De Gregorio <student@epicodus.com>
2015-08-20 11:14:15 -0700

    Allow user to add a book to the sinatra page.

--- app.rb
+++ app.rb
@@ -9,5 +9,16 @@ DB = PG.connect({:dbname => "library_system_test"})
 
 get('/') do
   @books = Book.all()
+
   erb(:index)
+end
+
+post('/new_book') do
+  book_title = params.fetch('new_title')
+  book_author = params.fetch('new_author')
+  book_genre = params.fetch('new_genre')
+  new_book = Book.new({:book_title => book_title, :book_author => book_author, :book_genre => book_genre, :book_id => nil})
+  new_book.save()
+
+  redirect('/')
 end
--- spec/library_integration_spec.rb
+++ spec/library_integration_spec.rb
@@ -3,9 +3,19 @@ require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions,false)
 
-describe('the list path', {:type => :feature}) do
+describe('the book path', {:type => :feature}) do
   it('allows the user to see a list of all books') do
     visit('/')
     expect(page).to have_content('Welcome to the Library')
   end
+
+  it('allows the user to add a book to the list') do
+    visit('/')
+    fill_in('new_title', :with => 'The Great Gatsby')
+    fill_in('new_author', :with => 'F. Scott Fitzgerald')
+    fill_in('new_genre', :with => 'Fiction')
+    click_button('Add book!')
+    expect(page).to have_content('The Great Gatsby')
+  end
+
 end
